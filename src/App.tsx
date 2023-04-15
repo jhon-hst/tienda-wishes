b478f3f54ff5e9f950abefa231ca3390f5e297c6
Jhon Sanchez <jhonhst2@gmail.com>
2023-04-15 10:05:24 -0500

    added styles in the header

--- src/App.tsx
+++ src/App.tsx
@@ -8,56 +8,47 @@ import "swiper/css/navigation";
 import "./styles.css";
 
 import testImg from "./assets/images/sku-1/sku-1-1-1.jpg";
+import granRemate from "./assets/gran-remate.jpeg";
 import Product from "./components/Products";
+import ProductsList from "./assets/data/products.json";
+import Images from "./assets/images";
 
 export default function App() {
   return (
     <main className="container pb-5">
       <div className="header">
-        <h1>Gram remate de productos</h1>
-        <p>
+        <div className="auction-container">
+          <img src={granRemate} className="auction-img" />
+        </div>
+        <h1 className="header-title">Gram remate de productos</h1>
+        <p className="header-description">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe
           similique facilis neque repudiandae aperiam blanditiis, id reiciendis
           harum quo eius fugiat ipsa rem accusantium voluptatibus aliquid
           expedita, cum quidem. Dolore.
         </p>
         <Swiper
-          slidesPerView={window.innerWidth > 1000 ? 5 : 1}
+          slidesPerView={window.innerWidth > 992 ? 5 : 1}
           spaceBetween={30}
           pagination={{
             type: "fraction",
           }}
           navigation={true}
           modules={[Pagination, Navigation]}
-          className="headerSwiper"
+          className="header-swiper"
         >
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={testImg} />
-          </SwiperSlide>
+          {ProductsList[0].media.map((data, i) => (
+            <>
+              {data.localImage && (
+                <SwiperSlide key={i}>
+                  <img
+                    src={Images[data.url]}
+                    className="img-swiper-slide-header"
+                  />
+                </SwiperSlide>
+              )}
+            </>
+          ))}
         </Swiper>
       </div>
       <Product />
