5a6040dbcf3464b31ea8d4c19c7dc3ffe6e20e47
Jhon Sanchez <jhonhst2@gmail.com>
2023-04-16 16:58:12 -0500

    added some images

---
+++ src/assets/images/sku-48/index.ts
@@ -0,0 +1,1 @@
+// https://video.aliexpress-media.com/play/u/ae_sg_item/2659665806/p/1/e/6/t/10301/315365921651.mp4?from=chrome&definition=h265
---
+++ src/assets/images/sku-52/index.ts
@@ -0,0 +1,1 @@
+// https://video.aliexpress-media.com/play/u/ae_sg_item/2201221609539/p/1/e/6/t/10301/1100005560759.mp4?from=chrome&definition=h265
